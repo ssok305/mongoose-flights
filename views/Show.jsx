0690b654124dfb0045150ed35c12173fa983210b
ssok305 <sutherysok@gmail.com>
2023-08-15 11:48:16 -0600

    Display airport and arrival

--- views/Show.jsx
+++ views/Show.jsx
@@ -1,38 +1,48 @@
 import React from "react";
 
-const Show = ({Flight}) => {
-    let {airline, flightNo, departs,  } = Flight
-    return(
-        <div>
+const Show = ({ Flight }) => {
+  let { airline, flightNo, departs, destinations } = Flight;
+  return (
+    <div>
+      <br />
+      {airline}
+      <br />
+      {flightNo}
+      <br />
+      {departs.toLocaleDateString() + " "}
+      {departs.toLocaleTimeString()}
+      <br />
+      {destinations
+        ? destinations.map((destination) => {
+            return (
+              <>
+                <p>{destination.arrivalAirport}</p>
+                <br />
+                <p>{destination.arrival.toLocaleDateString() + " "}</p>
+                <p>{destination.arrival.toLocaleTimeString()}</p>
+              </>
+            );
+          })
+        : "bye"}
+      <p>Arrival Information: </p>
+      <form method="POST" action={`/flights/${Flight._id}?_method=PUT`}>
+        <label htmlFor="Airport">Airports: </label>
+        <select name="arrivalAirport" id="Airport">
+          <option value="AUS">AUS</option>
+          <option value="DAL">DAL</option>
+          <option value="LAX">LAX</option>
+          <option value="SAN">SAN</option>
+          <option value="SEA">SEA</option>
+        </select>
+        <label htmlFor="Arrival"> Arrival Time: </label>
+        <input name="arrival" id="arrival" type="datetime-local" />
+        <button>Submit</button>
+      </form>
+      <a href="/flights">
+        <button>Back</button>
+      </a>
+    </div>
+  );
+};
 
-            <br/>
-            {airline}
-            <br/>
-            {flightNo}
-            <br/>
-            {departs.toLocaleDateString() + ' '}        
-            {departs.toLocaleTimeString()}
-            <br/>
-     
-            <p>Arrival Information: </p>
-            <form method="POST" action={`/flights/${Flight._id}?_method=PUT`} >
-                <label htmlFor="Airport">Airports: </label>
-                <select name="arrivalAirport" id="Airport">
-                        <option value='AUS'>AUS</option>
-                        <option value='DAL'>DAL</option>
-                        <option value='LAX'>LAX</option>
-                        <option value='SAN'>SAN</option>
-                        <option value='SEA'>SEA</option>
-                </select>
-                <label htmlFor="Arrival"> Arrival Time: </label>
-                <input name="arrival" id="arrival" type='datetime-local'/>
-                <button>Submit</button>
-                </form>
-            <a href="/flights">
-                <button>Back</button>
-            </a>
-        </div>
-    )
-}
-
-export default Show
+export default Show;
